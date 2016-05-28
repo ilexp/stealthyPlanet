392f1c9ca2b7fc8269adc7722cd9649208087816
j-p-e-g <no@mail.com>
2016-05-28 15:44:04 +0200

    Detect planet on raycast

--- Game/Source/Code/CorePlugin/EvilSpaceship.cs
+++ Game/Source/Code/CorePlugin/EvilSpaceship.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 using Duality;
 using Duality.Editor;
+using Duality.Components.Physics;
 
 namespace Game
 {
@@ -23,10 +24,12 @@ namespace Game
 		};
 
 		private ShipState m_shipState = ShipState.Uninitialized;
+		private float m_countdownToAttack = 0;
 
 		private float m_speed = 0.005f;
 		private float m_shootingDistance = 50000;
 		private float m_returnDistance = 400000;
+		private float m_waitingTimeInSeconds = 3;
 
 		public float ShootingDistance
 		{
@@ -65,6 +68,18 @@ namespace Game
 			}
 		}
 
+		public float WaitingTimeInSeconds
+		{
+			get
+			{
+				return this.m_waitingTimeInSeconds;
+			}
+			set
+			{
+				this.m_waitingTimeInSeconds = value;
+			}
+		}
+
 		public void OnInit(InitContext context)
 		{
 			if (context.Equals(InitContext.Activate))
@@ -96,6 +111,7 @@ namespace Game
 				}
 				break;
 			case ShipState.ScanTarget:
+				Log.Game.Write("distance: {0}", this.GameObj.Transform.Pos.LengthSquared);
 				if (ScanPlanet())
 				{
 					Log.Game.Write("Switch to ShootTarget state");
@@ -117,12 +133,18 @@ namespace Game
 				if (IsOutsideRange())
 				{
 					Log.Game.Write("Switch to Waiting state");
+					m_countdownToAttack = m_waitingTimeInSeconds;
 					m_shipState = ShipState.Waiting;
-				}
+                }
 				break;
 			case ShipState.Waiting:
-				Log.Game.Write("Switch to MoveToTarget state");
-				m_shipState = ShipState.MoveToTarget;
+				Log.Game.Write("countdownTime: (0.f), lastDelta: (1.f)", m_countdownToAttack, Time.LastDelta);
+				m_countdownToAttack -= Time.LastDelta;
+				if (m_countdownToAttack <= 0)
+				{
+					Log.Game.Write("Switch to MoveToTarget state");
+					m_shipState = ShipState.MoveToTarget;
+				}
 				break;
 			default:
 				break;
@@ -144,6 +166,21 @@ namespace Game
 		// returns false if the planet is shielded
 		bool ScanPlanet()
 		{
+			RayCastData firstHit;
+
+			VisualLog.Default.DrawPoint(this.GameObj.Transform.Pos.X, this.GameObj.Transform.Pos.Y, this.GameObj.Transform.Pos.Z).KeepAlive(1000);
+
+			Vector2 raycastEnd = (this.GameObj.Transform.Pos + m_direction).Xy;
+			RigidBody.RayCast(this.GameObj.Transform.Pos.Xy, raycastEnd, arg=>{	return arg.Fraction; }, out firstHit);
+
+			VisualLog.Default.DrawConnection(this.GameObj.Transform.Pos.X, this.GameObj.Transform.Pos.Y, this.GameObj.Transform.Pos.Z, firstHit.Pos.X, firstHit.Pos.Y).KeepAlive(1000);
+
+			if (firstHit.Body != null && firstHit.GameObj.GetComponent<Planet>() != null)
+			{
+				// Planet was hit!
+				return true;
+			}
+			// Hit something else (most likely the shield)
 			return false;
 		}
     }
---
+++ Game/Source/Code/CorePlugin/Planet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Duality;
+using Duality.Drawing;
+using Duality.Components;
+using Duality.Resources;
+
+namespace Game
+{
+	public class Planet : Component
+	{
+	}
+}
