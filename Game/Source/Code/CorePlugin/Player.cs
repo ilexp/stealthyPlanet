2d4e312c3f10ad2176f1b787a0c6096a6bdefcf7
Adam <adam@adamslair.net>
2016-05-28 16:16:05 +0200

    Content, Camera Controls

--- Game/Source/Code/CorePlugin/Player.cs
+++ Game/Source/Code/CorePlugin/Player.cs
@@ -12,11 +12,23 @@ namespace Game
 	public class Player : Component, ICmpUpdatable
 	{
 		private GameObject planetDisguise = null;
+		private CameraController camController = null;
+		private float cameraMovementStrength = 0.25f;
 
 		public GameObject PlanetDisguise
 		{
 			get { return this.planetDisguise; }
 			set { this.planetDisguise = value; }
+		}
+		public CameraController CamController
+		{
+			get { return this.camController; }
+			set { this.camController = value; }
+		}
+		public float CamMoveStrength
+		{
+			get { return this.cameraMovementStrength; }
+			set { this.cameraMovementStrength = value; }
 		}
 
 		public void OnUpdate()
@@ -27,6 +39,10 @@ namespace Game
 			if (this.planetDisguise != null)
 			{
 				this.planetDisguise.Transform.Angle = mouseAngle;
+			}
+			if (this.camController != null)
+			{
+				this.camController.TargetFocusPos = new Vector3(mouseRelativeToCenter * this.cameraMovementStrength, 0.0f);
 			}
 		}
 	}
